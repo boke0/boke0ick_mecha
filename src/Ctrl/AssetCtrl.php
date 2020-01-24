01c4a44db6549ee3654f8d74cacf667bb0d8ab25
木瓜丸 <boke0@eugensite.com>
2020-01-25 03:30:45 +0900

    [update] Pluginにassetを配置できる使用を追加/Endpointのtwigにcsrfフィールドを追加

--- src/Ctrl/AssetCtrl.php
+++ src/Ctrl/AssetCtrl.php
@@ -6,24 +6,42 @@ use \Boke0\Mechanism\Mdl;
 class AssetCtrl extends Ctrl{
     public function handle($req,$arg){
         $qs=$req->getQueryParams();
-        $type=isset($qs["type"])?$qs["type"]:NULL;
+        $type=isset($qs["type"])?$qs["type"]:FALSE;
         $filename=urldecode($qs["filename"]);
         $res=$this->createResponse();
-        if($type=="theme"||isset($qs["theme"])){
-            $themeMdl=new Mdl\Theme($qs["theme"],[],$this->container->get("plugin"));
-            if($themeMdl->assetExists($filename)){
-                return $themeMdl->asset($res,$filename);
+        if(!$type){
+            if(isset($qs["theme"])){
+                $type="theme";
+            }else if(isset($qs["plugin"])){
+                $type="plugin";
             }else{
-                return $res->withStatus(404,"Not Found");
+                $type="static";
             }
-        }else if($type="static"||file_exists(__DIR__."/../../static/{$filename}")){
-            $body=$res->getBody();
-            $body->write(
-                file_get_contents(__DIR__."/../../static/{$filename}")
-            );
-            $mime=$themeMdl->mimeContentType(__DIR__."/../../static/{$filename}");
-            return $res->withBody($body)
-                       ->withHeader("Content-Type",$mime);
+        }
+        switch($type){
+            case "theme":
+                $themeMdl=new Mdl\Theme($qs["theme"],[],$this->container->get("plugin"));
+                if($themeMdl->assetExists($filename)){
+                    return $themeMdl->asset($res,$filename);
+                }else{
+                    return $res->withStatus(404,"Not Found");
+                }
+            case "static":
+                $body=$res->getBody();
+                $body->write(
+                    file_get_contents(__DIR__."/../../static/{$filename}")
+                );
+                $mime=$themeMdl->mimeContentType(__DIR__."/../../static/{$filename}");
+                return $res->withBody($body)
+                           ->withHeader("Content-Type",$mime);
+            case "plugin":
+                $body=$res->getBody();
+                $body->write(
+                    file_get_contents(__DIR__."/../../plugins/{$qs["plugin"]}/assets/{$filename}")
+                );
+                $mime=$themeMdlr>mimeContentType(__DIR__."/../../plugins/{$qs["plugin"]}/assets/{$filename}");
+                return $res->withBody($body)
+                           ->withHeader("Content-Type",$mime);
         }
         return $res->withStatus(404,"Not Found");
     }
