d809fea7404967f1e42d89c990aba874d6a65c59
木瓜丸 <boke0@eugensite.com>
2020-01-29 02:33:45 +0900

    [fix] セッションを明示的に開始/配列マージの不具合を修正

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 require_once(__DIR__."/../vendor/autoload.php");
 
 use Boke0\Scapula\App;
--- src/Mdl/Plugin.php
+++ src/Mdl/Plugin.php
@@ -11,8 +11,8 @@ class Plugin extends Mdl{
         foreach($plugin_dirs as $dir){
             if(file_exists($plugin_dir_path.$dir."/__construct.php")){
                 $plugin=require($plugin_dir_path.$dir."/__construct.php");
-                $this->endpoints+=$plugin->getEndpoints();
-                $this->extension+=$plugin->getTemplateExtensions();
+                $this->endpoints=array_merge($this->endpoints,$plugin->getEndpoints());
+                $this->extension=array_merge($this->extension,$plugin->getTemplateExtensions());
             }
         }
     }
